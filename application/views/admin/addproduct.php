9fa1c3d2c08bcfd0dc5f11eeb3fb9174c5cff73e
huynhkimdai <daihuynhtrong@viendong.edu.vn>
2019-05-16 23:05:51 +0700

    chỉnh sửa vài chỗ

--- application/views/admin/addproduct.php
+++ application/views/admin/addproduct.php
@@ -4,40 +4,42 @@
 
 
 
-<div class="container">
+<div class="container mt-5">
+	<h1 class="text-center color-tim">Thêm Sản Phẩm</h1>
 	<div class="form-add-product">
  	 <form action="<?=base_url();?>sachController/InsertProduct" method="POST" enctype="multipart/form-data">
 
  	 	<table id="table-add-product">
+
  	 		 <tr>
  	 		 	<td>Name:</td>
- 	 		 	<td><input type="text" size="25" name="name_product"></td>
+ 	 		 	<td><input type="text" size="25" name="name_product" required="required"></td>
  	 		 </tr>
  	 		 <tr>
- 	 		 	<td>Miêu Tả</td>
- 	 		 	<td><textarea name="description"></textarea></td>
+ 	 		 	<td>Miêu Tả:</td>
+ 	 		 	<td><textarea  name="description" required="required"></textarea></td>
  	 		 </tr>
  	 		 <tr>
- 	 		 	<td>Giá</td>
- 	 		 	<td><input type="text" size="25" name="unit_price"></td>
+ 	 		 	<td>Giá:</td>
+ 	 		 	<td><input type="text" size="25" name="unit_price" required="required"></td>
  	 		 </tr>
  	 		 <tr>
- 	 		 	<td>Giá Khuyến mãi</td>
- 	 		 	<td><input type="text" size="25" name="promotion_price" ></td>
+ 	 		 	<td>Giá Khuyến mãi:</td>
+ 	 		 	<td><input type="text" size="25" name="promotion_price" required="required" ></td>
  	 		 </tr>
  	 		 <tr>
- 	 		 	<td>đơn vị</td>
- 	 		 	<td><input type="text" size="25" name="unit" ></td>
+ 	 		 	<td>đơn vị:</td>
+ 	 		 	<td><input type="text" size="25" name="unit" required="required"></td>
  	 		 </tr>
  	 		 <tr>
- 	 		 	<td>Hình ảnh</td>
- 	 		 	<td><input type="file" name="image" placeholder="Tên sản phẩm"></td>
+ 	 		 	<td>Hình ảnh:</td>
+ 	 		 	<td><input type="file" name="image" placeholder="Tên sản phẩm" required="required"></td>
  	 		 </tr>
  	 		 <tr>
  	 		 	<td></td>
  	 		 	<td><input class="btn btn-success add-product" type="submit" name="insert" value="Add"></td>
  	 		 </tr>
- 	 		 <?php 
+ 	 		  <?php 
 					if($this->uri->segment(2)=="insertedAddProduct") {
 						echo '<p class="text-success text-center">Success</p>';
 					}
