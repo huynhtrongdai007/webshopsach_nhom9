f6f348d13cc125ed8f7e3799f281903c078c004a
Thanh tam <33248219+thanhtam123456@users.noreply.github.com>
2019-06-06 10:24:16 +0700

    a

--- application/views/admin/product.php
+++ application/views/admin/product.php
@@ -1,4 +1,4 @@
-fj<?php 
+<?php 
     include'templates/header.php';
  ?>
 
@@ -6,22 +6,24 @@ fj<?php
 <h1 class="text-center display-4 color-tim">Danh Sách Sản Phẩm</h1>
 <hr class="w-50 bg-primary">
     <table class="table table-dark table-hover table-striped" id="users">
-    <thead class="name">
-        <th>STT</th>
-        <th>ID</th>
-        <th>Tên Sách</th>
-        <th>Mô tả</th>
-        <th>giá</th>
-        <th> Giákhuến mãi</th>
-        <th>Hình ảnh</th>
-        <th><a href="<?=base_url();?>sachController/addProduct">Them san pham</a></th>
+    <thead class="name" >
+        <th style="font-size: 15px">STT</th>
+        <th style="font-size: 15px">ID</th>
+        <th style="font-size: 15px">Tên Sách</th>
+        <th style="font-size: 15px">Mô tả</th>
+        <th style="font-size: 15px">giá</th>
+        <th style="font-size: 15px"> Giảm giá </th>
+        <th style="font-size: 15px">Hình ảnh</th>
+        <th style="font-size: 15px">
+            <button><a href="<?=base_url();?>sachController/addProduct">add</a></button>
+        </th>
 
     </thead>
     <tbody>
          <?php $stt=1; ?>
         <?php foreach ($data as $product):?>
         <tr>
-            <td><?= $stt; ?></td>
+            <td ><?= $stt; ?></td>
             <td><?= $product->id ?></td>
             <td><?= $product->name;?></td>
             <td><?= $product->description;?></td>
